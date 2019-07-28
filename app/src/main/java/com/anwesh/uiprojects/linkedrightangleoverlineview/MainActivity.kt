18f394c9a59c94d5acbb323ee7542edde9d444ae
Anwesh43 <anweshthecool0@gmail.com>
2019-07-28 20:04:35 +0530

    adding view in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrightangleoverlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrightangleoverlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedrightangleoverlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.rightangleoverlineview.RightAngleOverLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RightAngleOverLineView.create(this)
     }
 }
