312537b8d521536021e16817ea81081e578e2cec
Anwesh43 <anweshthecool0@gmail.com>
2019-07-28 18:39:08 +0530

    adding libraries and constants

--- rightangleoverlineview/src/main/java/com/anwesh/uiprojects/rightangleoverlineview/RightAngleOverLineView.kt
+++ rightangleoverlineview/src/main/java/com/anwesh/uiprojects/rightangleoverlineview/RightAngleOverLineView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.rightangleoverlineview
  * Created by anweshmishra on 28/07/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 2
+val parts : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#3949AB")
+val backColor : Int = Color.parseColor("#BDBDBD")
