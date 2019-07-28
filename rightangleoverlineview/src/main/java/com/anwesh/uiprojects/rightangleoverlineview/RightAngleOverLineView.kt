2bbbb7ef3c6410e14df06a1b349a9c6a20c2e063
Anwesh43 <anweshthecool0@gmail.com>
2019-07-28 19:32:06 +0530

    adding Canvas extension methods

--- rightangleoverlineview/src/main/java/com/anwesh/uiprojects/rightangleoverlineview/RightAngleOverLineView.kt
+++ rightangleoverlineview/src/main/java/com/anwesh/uiprojects/rightangleoverlineview/RightAngleOverLineView.kt
@@ -31,3 +31,38 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawRightAngleLine(i : Int, sc : Float, size : Float, paint : Paint) {
+    val sci : Float = sc.divideScale(i, lines)
+    val sf : Float = 1f - 2 * i
+    for (j in 0..parts) {
+        val scj : Float = sci.divideScale(j, parts)
+        val x1j : Float = size / 2 * j
+        val x2j : Float = size / 2 * j + (1 - j) * (size / 2) * scj
+        val y2j : Float = size / 2 * j * scj
+        save()
+        translate(0f, (size / 2) * sf)
+        drawLine(x1j, 0f, x2j, y2j, paint)
+        restore()
+    }
+}
+
+fun Canvas.drawRAOLNode(i : Int, scale : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = Math.min(w, h) / strokeFactor
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor 
+    save()
+    translate(gap * (i + 1), h / 2)
+    rotate(90f * sc2)
+    drawLine(0f, -size / 2, 0f, size / 2)
+    for (j in 0..(lines - 1)) {
+        drawRightAngleLine(j, sc1, size, paint)
+    }
+    restore()
+}
